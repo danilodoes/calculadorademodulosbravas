18f3513c5c98d35b9afe58762702511892e1e7ba
Danilo dos Santos <danilodoes@hotmail.com>
2025-07-14 17:40:15 -0300

    regras de negocios para calculo dos módulos

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
 document.addEventListener("DOMContentLoaded", function () {
+  //Quantidade de Módulos
+  let humModAcesso = 4;
+  let qtdModAcesso = 0; let qtdMod2x10 = 0;
+  let qtdModRF = 0; let qtdModRFIP = 0;
+  let qtdModPorta = 0; let qtdModPortaIP = 0;
+  let qtdMod4x4 = 0; let qtdMod4x4IP = 0;
+  let qtdModCatraca = 0; let qtdModCatracaIP = 0;
 
+
+  //Função para resetar o valor das variáveis
+
+  function resetarVariaveis() {
+    humModAcesso = 4;
+    qtdModAcesso = 0; qtdMod2x10 = 0;
+    qtdModRF = 0; qtdModRFIP = 0;
+    qtdModPorta = 0; qtdModPortaIP = 0;
+    qtdMod4x4 = 0; qtdMod4x4IP = 0
+    qtdModCatraca = 0; qtdModCatracaIP = 0;
+  };
+
+
+
+  // Botão Calcular
   const btnCalcular = document.querySelector(".calcular");
   btnCalcular.addEventListener("click", () => {
+
+    // Radios e Switches
+    const tipoComunicacao = document.querySelector('input[name="rd_Comunicacao"]:checked').value;
+    const vaiTerAcessoVeicular = document.querySelector('input[name="rd_checkAcessoVeicular"]:checked').value;
+    const vaiTerAcessoVeicularRF = document.querySelector('input[name="rd_checkControleRemoto"]:checked').value;
+    const vaiTerCatraca = document.querySelector('input[name="rd_checkCatraca"]:checked').value;
+    const vaiTerAutomacao = document.querySelector('input[name="rd_checkAutomacao"]:checked').value;
+    const automacaoElevadores = document.querySelector('input[id="elevador"]').checked;
+    const automacaoAntiCarona = document.querySelector('input[id="antiCarona"]').checked;
+    const automacaoControleVagas = document.querySelector('input[id="controleVaga"]').checked;
+    const controleRemotoSim = document.querySelector("#rd_controleRemotoSim");
+
+    // Text fields (inputaveis)
     const acessosTotais = document.querySelector(".tfAcessosTotais").value || 0;
     const acessosVeicularesTotais = document.querySelector(".tfAcessosVeicularesTotais").value || 0;
     const acessosVeicularesRF = document.querySelector(".tfAcessosVeicularesRF").value || 0;
@@ -12,92 +47,140 @@ document.addEventListener("DOMContentLoaded", function () {
     const qtdControleVagasEntrada = document.querySelector(".tfQtdControleVagasEntrada").value || 0;
     const qtdControleVagasSaida = document.querySelector(".tfQtdControleVagasSaida").value || 0;
 
-  
-    console.log(`Acessos Totais: ${acessosTotais}`);
-    console.log(`Acessos Veiculares Totais: ${acessosVeicularesTotais}`);
-    console.log(`Acessos Veiculares RF: ${acessosVeicularesRF}`);    
-    console.log(`Acessos por Catraca: ${acessosCatraca}`);
-    console.log(`Quantidade de Elevadores: ${qtdElevadores}`);
-    console.log(`Quantidade de Andares: ${qtdAndares}`);
-    console.log(`Quantidade de Acessos Anti-Caronas: ${qtdAcessosAntiCarona}`);
-    console.log(`Quantidade de Acessos Controle de Vagas Entrada: ${qtdControleVagasEntrada}`);
-    console.log(`Quantidade de Acessos Controle de Vagas Saída: ${qtdControleVagasSaida}`);
 
 
+    //Quantidade de Módulos
+    qtdModPorta = qtdModPortaIP = (acessosTotais - acessosCatraca - humModAcesso - acessosVeicularesRF);
+    qtdModCatraca = qtdModCatracaIP = acessosCatraca;
+
+
+    if (tipoComunicacao === "tcp") {
+      //Quantidade de Módutos Porta IP
+      if (qtdModPortaIP > 0) {
+        console.log(`${qtdModPortaIP} UN - Módulo Porta IP (PRD0008)`);
+      };
+
+      //Quantidade de Módulos Catraca IP
+      if (qtdModCatracaIP > 0) {
+        console.log(`${qtdModCatracaIP} UN - Módulo Catraca IP (PRD0016)`);
+      };
+
+      //Quantidade de Módulos RF IP
+      if (acessosVeicularesRF > 0 && acessosVeicularesRF <= 4) {
+        qtdModRFIP = 1;
+        console.log(`${qtdModRFIP} UN - Módulo RF IP (PRD0009)`);
+      } else if (acessosVeicularesRF >= 5 && acessosVeicularesRF <= 8) {
+        qtdModRFIP = 2;
+        console.log(`${qtdModRFIP} UN - Módulo RF IP (PRD0009)`);
+      };
+
+      //Automações: Elevadores
+      if (vaiTerAutomacao) {
+        qtdMod4x4IP = Math.ceil(qtdAndares / 4) * (qtdElevadores);
+        qtdModAcesso += Math.ceil(qtdMod4x4IP / 40);
+        console.log(`${qtdMod4x4IP} UN - Módulo PGM 4x4 IP (PRD0013)`);
+      };
+
+      //Automações: Anti-Carona
+      if (automacaoAntiCarona) {
+      }
+
+      //Automações: Controle de Vagas
+      if (automacaoControleVagas) {
+      }
+
+
+      console.log(`${qtdModAcesso} UN - Módulo Acesso Programável (PRD0028)`);
+
+      /////////////// SERIAL 485 /////////////// SERIAL 485 /////////////// SERIAL 485
+    } else if (tipoComunicacao === "serial") {
+      //Quantidade de Módulos Porta RS-485
+      if (qtdModPorta > 0) {
+        console.log(`${qtdModPorta} UN - Módulo Porta RS-485 (PRD0011)`);
+      };
+
+      //Quantidade de Módulos Catraca RS-485
+      if (qtdModCatraca > 0) {
+        console.log(`${qtdModCatraca} UN - Módulo Catraca RS-485 (PRD0010)`);
+      };
+
+      //Quantidade de Módulos RF RS-485
+      if (acessosVeicularesRF > 0 && acessosVeicularesRF <= 4) {
+        qtdModRF = 1;
+        console.log(`${qtdModRF} UN - Módulo RF RS-485 (PRD0018)`);
+      } else if (acessosVeicularesRF >= 5 && acessosVeicularesRF <= 8) {
+        qtdModRF = 2;
+        console.log(`${qtdModRF} UN - Módulo RF RS-485 (PRD0018)`);
+      };
+
+      //Automações: Elevadores
+      if (vaiTerAutomacao) {
+        qtdModAcesso += qtdElevadores;
+        qtdMod2x10 = Math.floor(qtdAndares / 10) * (qtdElevadores);
+
+        if (qtdAndares % 10 >= 1 && qtdAndares % 10 <= 4) {
+          qtdMod4x4 = (1 * qtdElevadores);
+
+        } else if (qtdAndares % 10 >= 5 && qtdAndares % 10 <= 9) {
+          qtdMod4x4 = 0;
+          qtdMod2x10 += (1 * qtdElevadores);
+
+        } else {
+          qtdMod4x4 = 0;
+        };
+        if (qtdMod4x4 != 0) {
+
+          console.log(`${qtdMod4x4} UN - Módulo PGM 4x4 RS-485 (PRD0014)`);
+        };
+
+        console.log(`${qtdMod2x10} UN - Módulo PGM 2x10 RS-485 (PRD0063)`);
+
+
+      };
+
+      //Automações: Anti-Carona
+      if (automacaoAntiCarona) {
+      }
+
+      //Automações: Controle de Vagas
+      if (automacaoControleVagas) {
+      }
+
+      console.log(`${qtdModAcesso} UN - Módulo Acesso Programável (PRD0028)`);
+
+
+
+    };
+
+
+
+
+
+
+
+
+
+    console.log(`Quantidade de acessos totais: ${acessosTotais}`);
+    console.log(`Tipo de comunicação: ${tipoComunicacao}`);
+    console.log(`Haverá acessos veiculares? ${vaiTerAcessoVeicular}`);
+    console.log(`Quantidade de acessos veiculares totais: ${acessosVeicularesTotais}`);
+    console.log(`Haverá acessos veiculares RF? ${vaiTerAcessoVeicularRF}`);
+    console.log(`Acessos veiculares RF: ${acessosVeicularesRF}`);
+    console.log(`Haverá acessos por catracas? ${vaiTerCatraca}`);
+    console.log(`Quantidade de acessos por catraca: ${acessosCatraca}`);
+    console.log(`Haverá automações? ${vaiTerAutomacao}`);
+    console.log(`Haverá automação de elevadores? ${automacaoElevadores}`);
+    console.log(`Quantidade de elevadores: ${qtdElevadores}`);
+    console.log(`Quantidade de andares: ${qtdAndares}`);
+    console.log(`Haverá automação de anti-carona? ${automacaoAntiCarona}`);
+    console.log(`Quantidade de acessos anti-caronas: ${qtdAcessosAntiCarona}`);
+    console.log(`Haverá automação de controle de cagas? ${automacaoControleVagas}`);
+    console.log(`Quantidade de acessos controle de vagas entrada: ${qtdControleVagasEntrada}`);
+    console.log(`Quantidade de acessos controle de vagas saída: ${qtdControleVagasSaida}`);
+
+    resetarVariaveis();
 
   });
-
-  // Dados inputaveis: Tipos de acessos
-  const veicular = false;
-
-  // Dados inputaveis: Automações
-  const automacoes = true;
-  const elevador = true;
-  const anticarona = false;
-  const controleVagas = false;
-
-  // Dados inputaveis: Tipos de Conexão
-  const tcp = true;
-  const serial = false;
-
-  // Dados inputaveis: Gerais
-  let qtdElevadores = 4;
-  let qtdAndares = 12;
-  let qtdAcessosTotais = 0;
-  let qtdAcessoRF = 0;
-  let qtdAcessoCatraca = 0;
-  // ===============================================
-
-
-  // Quantidade de Módulos
-  let qtdModAcesso = 0; let qtdMod2x10 = 0;
-  let qtdModRF = 0; let qtdModRFIP = 0;
-  let qtdModPorta = 0; let qtdModPortaIP = 0;
-  let qtdMod4x4 = 0; let qtdMod4x4IP = 0;
-  let qtdModCatraca = 0; let qtdModCatracaIP = 0;
-  // ===============================================
-
-
-  // Definindo quantos módulos serão utilizados para automação de elevadores
-  if (elevador) {
-    if (tcp) {
-      qtdMod4x4IP = Math.ceil(qtdAndares / 4) * (qtdElevadores);
-      qtdModAcesso = Math.ceil((qtdMod4x4IP / 40));
-    };
-
-    if (serial) {
-      qtdModAcesso += qtdElevadores;
-      qtdMod2x10 = Math.floor(qtdAndares / 10) * (qtdElevadores);
-
-      if (qtdAndares % 10 >= 1 && qtdAndares % 10 <= 4) {
-        qtdMod4x4 = (1 * qtdElevadores);
-
-      } else if (qtdAndares % 10 >= 5 && qtdAndares % 10 <= 7) {
-        qtdMod4x4 = (2 * qtdElevadores);
-
-      } else if (qtdAndares % 10 >= 8 && qtdAndares % 10 <= 9) {
-        qtdMod4x4 = 0;
-        qtdMod2x10 += (1 * qtdElevadores);
-
-      } else {
-        qtdMod4x4 = 0;
-      }
-    };
-  };
-  // ===============================================
-
-  // Definindo quantos módulos catracas
-  if (qtdAcessoCatraca > 0) {
-    if (tcp) {
-      qtdModCatracaIP += qtdAcessoCatraca;
-      qtdModAcesso += 1;
-
-    } else if (serial) {
-      qtdModCatraca += qtdAcessoCatraca;
-      qtdModAcesso += 1;
-    };
-  };
-  // ===============================================
 
 
   // Saída para o usuário
--- controller.js
+++ controller.js
@@ -1,139 +1,139 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-         // Função para monitorar os switches
-        function monitorarSwitches(nomeDoSwitch, callback) {
-            const switches = document.querySelectorAll(`input[id="${nomeDoSwitch}"]`);
-            switches.forEach(switchElement => {
-                switchElement.addEventListener("change", () => {
+    // Função para pegar o estado dos rádios
+    function monitorarRadios(nomeDoGrupo, callback) {
+        const radios = document.querySelectorAll(`input[name="${nomeDoGrupo}"]`);
+        radios.forEach(radio => {
+            radio.addEventListener("change", () => {
+                const selecionado = document.querySelector(`input[name="${nomeDoGrupo}"]:checked`);
+                callback(selecionado.value);
+            });
+        });
+    };
+
+    // Função para monitorar os switches
+    function monitorarSwitches(nomeDoSwitch, callback) {
+        const switches = document.querySelectorAll(`input[id="${nomeDoSwitch}"]`);
+        switches.forEach(switchElement => {
+            switchElement.addEventListener("change", () => {
                 callback(switchElement.checked);
-                });
             });
+        });
+    };
+
+
+    // Tipo de Comunicação
+    monitorarRadios("rd_Comunicacao", (valor) => {
+        // console.log("Comunicação selecionada:", valor);
+    });
+
+    // Radios: Acesso Veiculares
+    monitorarRadios("rd_checkAcessoVeicular", (valor) => {
+        const divQtdAcessosVeiculares = document.getElementById("qtdAcessosVeiculares");
+        const divQtdAcessosVeicularesRF = document.getElementById("qtdAcessosVeicularesRF");
+        if (valor === "true") {
+            divQtdAcessosVeiculares.classList.remove("d-none");
+            // console.log(`Acesso Veicular: ${valor}`);
+        } else {
+            divQtdAcessosVeiculares.classList.add("d-none");
+            divQtdAcessosVeicularesRF.classList.add("d-none");
+            divQtdAcessosVeicularesRF.value = "false";
+            // console.log(`Acesso Veicular: ${valor}`);
         };
+    });
 
-        
-        // Função para pegar o estado dos rádios
-        function monitorarRadios(nomeDoGrupo, callback) {
-            const radios = document.querySelectorAll(`input[name="${nomeDoGrupo}"]`);
-            radios.forEach(radio => {
-                radio.addEventListener("change", () => {
-                    const selecionado = document.querySelector(`input[name="${nomeDoGrupo}"]:checked`);
-                    callback(selecionado.value);
-                });
-            });
+    // Radios: Controle Remoto
+    monitorarRadios("rd_checkControleRemoto", (valor) => {
+        const divQtdAcessosVeicularesRF = document.getElementById("qtdAcessosVeicularesRF");
+        if (valor === "true") {
+            divQtdAcessosVeicularesRF.classList.remove("d-none");
+            // console.log(`Controle remoto: ${valor}`);
+        } else {
+            divQtdAcessosVeicularesRF.classList.add("d-none");
+            // console.log(`Controle remoto: ${valor}`);
         };
+    });
 
-        // Tipo de Comunicação
-        monitorarRadios("rd_Comunicacao", (valor) => {
-            console.log("Comunicação selecionada:", valor);
-        });
-
-        // Radios: Acesso Veiculares
-        monitorarRadios("rd_checkAcessoVeicular", (valor) => {
-            const divQtdAcessosVeiculares = document.getElementById("qtdAcessosVeiculares");
-            const divQtdAcessosVeicularesRF = document.getElementById("qtdAcessosVeicularesRF");
-            if (valor === "true") {
-                divQtdAcessosVeiculares.classList.remove("d-none");
-                console.log(`Acesso Veicular: ${valor}`);
-            } else {
-                divQtdAcessosVeiculares.classList.add("d-none");
-                divQtdAcessosVeicularesRF.classList.add("d-none");
-                divQtdAcessosVeicularesRF.value = "false";
-                console.log(`Acesso Veicular: ${valor}`);
-            };
-        });
-
-        // Radios: Controle Remoto
-        monitorarRadios("rd_checkControleRemoto", (valor) => {
-            const divQtdAcessosVeicularesRF = document.getElementById("qtdAcessosVeicularesRF");
-            if (valor === "true") {
-                divQtdAcessosVeicularesRF.classList.remove("d-none");
-                console.log(`Controle remoto: ${valor}`);
-            } else {
-                divQtdAcessosVeicularesRF.classList.add("d-none");
-                console.log(`Controle remoto: ${valor}`);
-            };
-        });
-
-        // Radios: Catraca
-        monitorarRadios("rd_checkCatraca", (valor) => {
-            const divQtdCatraca = document.getElementById("qtdCatraca");
-            if (valor === "true") {
-                divQtdCatraca.classList.remove("d-none");
-                console.log(`Catracas: ${valor}`);
-            } else {
-                divQtdCatraca.classList.add("d-none");
-                console.log(`Catracas: ${valor}`);
-            };
-
-        });
-
-        // Radios: Automações
-        monitorarRadios("rd_checkAutomacao", (valor) => {
-            const divQuaisAutomacoes = document.getElementById("quaisAutomacoes");
-            if (valor === "true") {
-                divQuaisAutomacoes.classList.remove("d-none");
-                console.log(`Automações: ${valor}`);
-            } else {
-                const switches = document.querySelectorAll(".form-check.form-switch");
-                switches.forEach(switchElement => {
-                    switchElement.querySelector("input").checked = false;
-                });    
-                divQuaisAutomacoes.classList.add("d-none"); 
-                console.log(`Automações: ${valor}`);
-            };
-        });
-
-
-        // Switches: Elevadores
-        monitorarSwitches("elevador", (checked) => {
-                
-            if (checked) {
-                const vaiTerElevadores = checked;
-                const containerElevadores = document.querySelector(".containerElevadores");
-                containerElevadores.classList.remove("d-none");
-                console.log(`Vai ter automação nos elevadores: ${vaiTerElevadores}`);                       
-            }else{
-                const vaiTerElevadores = checked;
-                const containerElevadores = document.querySelector(".containerElevadores");
-                containerElevadores.classList.add("d-none");
-                console.log(`Vai ter automação nos elevadores: ${vaiTerElevadores}`);
-            };           
-        });
-
-        // Switches: Anti-Carona
-        monitorarSwitches("antiCarona", (checked) => {
-            
-            if (checked) {
-                const vaiTerAntiCarona = checked;
-                const ContainerAntiCarona = document.querySelector(".containerAntiCarona");
-                ContainerAntiCarona.classList.remove("d-none");
-                console.log(`Vai ter automação de Anti Carona: ${vaiTerAntiCarona}`);
-        
-            } else {
-                const vaiTerAntiCarona = checked;
-                const ContainerAntiCarona = document.querySelector(".containerAntiCarona");
-                ContainerAntiCarona.classList.add("d-none");
-                console.log(`Vai ter automação de Anti Carona: ${vaiTerAntiCarona}`);
-        
-            };
-        });
-
-        // Switches: Controle de Vagas
-        monitorarSwitches("controleVaga", (checked) => {
-            
-            if (checked) {
-                const vaiTerControleVagas = checked;
-                const containerControleVagas = document.querySelector(".containerControleVagas");
-                containerControleVagas.classList.remove("d-none");
-                console.log(`Vai ter automação de Controle de Vagas: ${vaiTerControleVagas}`);
-            } else {
-                const vaiTerControleVagas = checked;
-                const containerControleVagas = document.querySelector(".containerControleVagas");
-                containerControleVagas.classList.add("d-none");
-                console.log(`Vai ter automação de Controle de Vagas: ${vaiTerControleVagas}`);
-            };
-        });
+    // Radios: Catraca
+    monitorarRadios("rd_checkCatraca", (valor) => {
+        const divQtdCatraca = document.getElementById("qtdCatraca");
+        if (valor === "true") {
+            divQtdCatraca.classList.remove("d-none");
+            // console.log(`Catracas: ${valor}`);
+        } else {
+            divQtdCatraca.classList.add("d-none");
+            // console.log(`Catracas: ${valor}`);
+        };
 
     });
+
+    // Radios: Automações
+    monitorarRadios("rd_checkAutomacao", (valor) => {
+        const divQuaisAutomacoes = document.getElementById("quaisAutomacoes");
+        if (valor === "true") {
+            divQuaisAutomacoes.classList.remove("d-none");
+            // console.log(`Automações: ${valor}`);
+        } else {
+            const switches = document.querySelectorAll(".form-check.form-switch");
+            switches.forEach(switchElement => {
+                switchElement.querySelector("input").checked = false;
+            });
+            divQuaisAutomacoes.classList.add("d-none");
+            // console.log(`Automações: ${valor}`);
+        };
+    });
+
+
+    // Switches: Elevadores
+    monitorarSwitches("elevador", (checked) => {
+
+        if (checked) {
+            const vaiTerElevadores = checked;
+            const containerElevadores = document.querySelector(".containerElevadores");
+            containerElevadores.classList.remove("d-none");
+            // console.log(`Vai ter automação nos elevadores: ${vaiTerElevadores}`);                       
+        } else {
+            const vaiTerElevadores = checked;
+            const containerElevadores = document.querySelector(".containerElevadores");
+            containerElevadores.classList.add("d-none");
+            // console.log(`Vai ter automação nos elevadores: ${vaiTerElevadores}`);
+        };
+    });
+
+    // Switches: Anti-Carona
+    monitorarSwitches("antiCarona", (checked) => {
+
+        if (checked) {
+            const vaiTerAntiCarona = checked;
+            const ContainerAntiCarona = document.querySelector(".containerAntiCarona");
+            ContainerAntiCarona.classList.remove("d-none");
+            // console.log(`Vai ter automação de Anti Carona: ${vaiTerAntiCarona}`);
+
+        } else {
+            const vaiTerAntiCarona = checked;
+            const ContainerAntiCarona = document.querySelector(".containerAntiCarona");
+            ContainerAntiCarona.classList.add("d-none");
+            // console.log(`Vai ter automação de Anti Carona: ${vaiTerAntiCarona}`);
+
+        };
+    });
+
+    // Switches: Controle de Vagas
+    monitorarSwitches("controleVaga", (checked) => {
+
+        if (checked) {
+            const vaiTerControleVagas = checked;
+            const containerControleVagas = document.querySelector(".containerControleVagas");
+            containerControleVagas.classList.remove("d-none");
+            // console.log(`Vai ter automação de Controle de Vagas: ${vaiTerControleVagas}`);
+        } else {
+            const vaiTerControleVagas = checked;
+            const containerControleVagas = document.querySelector(".containerControleVagas");
+            containerControleVagas.classList.add("d-none");
+            // console.log(`Vai ter automação de Controle de Vagas: ${vaiTerControleVagas}`);
+        };
+    });
+
+});
 
 
